ac8c885100877bde89222e662d805cd1dc843a54
Leopold Jacquot <leopold.jacquot@gmail.com>
2015-10-10 21:52:47 +0200

    fix and clean

--- front/app/scripts/app.config.js
+++ front/app/scripts/app.config.js
@@ -1,20 +1,42 @@
-(function() {
+(function () {
     'use strict';
 
     angular.module('hackNet')
-        .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-            $routeProvider.when('/login', {templateUrl: 'views/login.html', login: true});
-            $routeProvider.when('/signup', {templateUrl: 'views/signup.html', public: true});
-            $routeProvider.when('/home', {templateUrl: 'views/home.html', controller: 'MainCtrl', public: false});
-            $routeProvider.when('/verify-email', {templateUrl: 'verify-email.html', verify_email: true});
-            $routeProvider.when('/reset-password', {templateUrl: 'reset-password.html', public: true});
-            $routeProvider.when('/set-password', {templateUrl: 'set-password.html', set_password: true});
-            $routeProvider.otherwise({redirectTo: '/home'});
-        }])
+        .config(['$routeProvider',
+            '$locationProvider',
+            function ($routeProvider, $locationProvider) {
+                $routeProvider.when('/login', {
+                    templateUrl: 'views/login.html',
+                    login: true
+                });
+                $routeProvider.when('/signup', {
+                    templateUrl: 'views/signup.html',
+                    public: true
+                });
+                $routeProvider.when('/home', {
+                    templateUrl: 'views/home.html',
+                    controller: 'MainCtrl',
+                    public: false
+                });
+                $routeProvider.when('/verify-email', {
+                    templateUrl: 'views/verify-email.html',
+                    verify_email: true
+                });
+                $routeProvider.when('/reset-password', {
+                    templateUrl: 'views/reset-password.html',
+                    public: true
+                });
+                $routeProvider.when('/set-password', {
+                    templateUrl: 'views/set-password.html',
+                    set_password: true
+                });
+                $routeProvider.otherwise({redirectTo: '/home'});
+            }])
         .config(['terminalConfigurationProvider',
             function (terminalConfigurationProvider) {
 
-                terminalConfigurationProvider.config('vintage').outputDelay = 10;
+                terminalConfigurationProvider.config('vintage').outputDelay =
+                    10;
                 terminalConfigurationProvider.config('vintage').allowTypingWriteDisplaying =
                     false;
                 terminalConfigurationProvider.config('vintage').typeSoundUrl =
